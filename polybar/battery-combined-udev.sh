9a08c2e89ecfde83646bdedb626c4f000b017777
Nguyễn Sĩ Hùng <hung.ns@teko.vn>
2022-02-27 18:33:37 +0700

    #patched update dynamic wallpaper

--- polybar/battery-combined-udev.sh
+++ polybar/battery-combined-udev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 battery_print() {
-    path_ac="/sys/class/power_supply/AC0"
+    path_ac="/sys/class/power_supply/AC"
     path_battery_0="/sys/class/power_supply/BAT0"
     path_battery_1="/sys/class/power_supply/BAT1"
 
@@ -15,20 +15,20 @@ battery_print() {
         ac=$(cat "$path_ac/online")
     fi
 
-    if [ -f "$path_battery_0/energy_now" ]; then
-        battery_level_0=$(cat "$path_battery_0/energy_now")
+    if [ -f "$path_battery_0/charge_now" ]; then
+        battery_level_0=$(cat "$path_battery_0/charge_now")
     fi
 
-    if [ -f "$path_battery_0/energy_full" ]; then
-        battery_max_0=$(cat "$path_battery_0/energy_full")
+    if [ -f "$path_battery_0/charge_full" ]; then
+        battery_max_0=$(cat "$path_battery_0/charge_full")
     fi
 
-    if [ -f "$path_battery_1/energy_now" ]; then
-        battery_level_1=$(cat "$path_battery_1/energy_now")
+    if [ -f "$path_battery_1/charge_now" ]; then
+        battery_level_1=$(cat "$path_battery_1/charge_now")
     fi
 
-    if [ -f "$path_battery_1/energy_full" ]; then
-        battery_max_1=$(cat "$path_battery_1/energy_full")
+    if [ -f "$path_battery_1/charge_full" ]; then
+        battery_max_1=$(cat "$path_battery_1/charge_full")
     fi
 
     battery_level=$(("$battery_level_0 + $battery_level_1"))
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,6 +1,8 @@
 source $XDG_CONFIG_HOME/aliases/aliases
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/fzf/completion.zsh
+source /usr/share/fzf/key-bindings.zsh
 
 fpath+=$XDG_CONFIG_HOME/zsh/prompt/pure
 autoload -U promptinit; promptinit
@@ -14,3 +16,8 @@ if [ -f '/home/dagadark/google-cloud-sdk/path.zsh.inc' ]; then . '/home/dagadark
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/home/dagadark/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/dagadark/google-cloud-sdk/completion.zsh.inc'; fi
+
+# Add the warp directory  
+wd() {
+    . /usr/share/wd/wd.sh
+}
